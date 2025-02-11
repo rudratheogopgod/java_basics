1a3d402b225eb2b819b27b5ccd3ae3114b13ed65
rudratheogopgod <rudrasabsebesthai@gmail.com>
2025-02-11 23:09:22 +0530

    4th-Feb-2025 Java basics commit.

--- src/BitwiseOperator.java
+++ src/BitwiseOperator.java
@@ -6,7 +6,7 @@ public class BitwiseOperator {
         System.out.println(p | q); //11 Bitwise OR
         System.out.println(p & q); //8 Bitwise AND
         System.out.println(p ^ q); //3 Bitwise XOR
-        System.out.println(~p); //6 Bitwise Complement
+        System.out.println(~p); //-10 Bitwise Complement
         System.out.println(p<<2); //36 Bitwise Left Shift
         System.out.println(q<<1); //5 Bitwise Right Shift
     }
